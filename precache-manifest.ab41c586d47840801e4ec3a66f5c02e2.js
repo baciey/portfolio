877d9e94ba0910c9603e1d2bc15b624a7fabf112
baciey <baciey@gmail.com>
2020-06-09 10:10:35 +0200

    Updates

---
+++ precache-manifest.ab41c586d47840801e4ec3a66f5c02e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f3f04ba9fd02e9f182c",
+    "url": "/baciey/portfolio/static/css/main.f0a5b75f.chunk.css"
+  },
+  {
+    "revision": "4f3f04ba9fd02e9f182c",
+    "url": "/baciey/portfolio/static/js/main.06e66f96.chunk.js"
+  },
+  {
+    "revision": "e671f1af3cbc40332776",
+    "url": "/baciey/portfolio/static/js/runtime~main.744b0be1.js"
+  },
+  {
+    "revision": "c9349883cdbb5338d07e",
+    "url": "/baciey/portfolio/static/js/2.34218308.chunk.js"
+  },
+  {
+    "revision": "ad8c07821a70115026bd1370199eb190",
+    "url": "/baciey/portfolio/static/media/dietaCud.ad8c0782.png"
+  },
+  {
+    "revision": "1c144576fc68595a96527f7b1a1b63da",
+    "url": "/baciey/portfolio/static/media/dietaCud2.1c144576.png"
+  },
+  {
+    "revision": "31fb33af63eb92033f26f25aba6dafd3",
+    "url": "/baciey/portfolio/static/media/dbzMemory.31fb33af.png"
+  },
+  {
+    "revision": "3cadcbd7bff092f47893392d40e5547b",
+    "url": "/baciey/portfolio/static/media/dbzMemory2.3cadcbd7.png"
+  },
+  {
+    "revision": "06633ef772c8541bd15be41728f6d858",
+    "url": "/baciey/portfolio/static/media/monitor.06633ef7.png"
+  },
+  {
+    "revision": "d3255c179c7ea60b5364f3f82fbd73aa",
+    "url": "/baciey/portfolio/static/media/mountain.d3255c17.jpg"
+  },
+  {
+    "revision": "8079ab461f4e0764591c755befe5b376",
+    "url": "/baciey/portfolio/index.html"
+  }
+];
