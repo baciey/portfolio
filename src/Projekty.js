11e8199db36dbb624af842cf9182508f7c34f39b
baciey <baciey@gmail.com>
2020-06-09 10:17:40 +0200

    title change

--- src/Projekty.js
+++ src/Projekty.js
@@ -4,7 +4,6 @@ import DietaCud from './img/dietaCud.png'
 import DietaCud2 from './img/dietaCud2.png'
 import DbzMemory from './img/dbzMemory.png'
 import DbzMemory2 from './img/dbzMemory2.png'
-
 import Monitor from './img/monitor.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -12,7 +11,6 @@ import { faDatabase, faForward } from '@fortawesome/free-solid-svg-icons'
 import { faReact, faJs, faHtml5, faCss3Alt, faPhp } from "@fortawesome/free-brands-svg-icons"
 
 
-// faReact, faJs, faCss3Alt, faHtml5
 
 class Projekty extends Component {
   state = {
@@ -65,7 +63,7 @@ class Projekty extends Component {
       img2: DietaCud2,
       techs: [this.js, this.html, this.css, this.php, this.sql],
       link: 'https://dietacud.000webhostapp.com/',
-      gitLink: 'https://github.com/baciey/dbzMemoryGame',
+      gitLink: 'https://github.com/baciey/dietaCud',
     }
   ]
   projectChange = () => {
@@ -85,16 +83,16 @@ class Projekty extends Component {
     const techs = this.projects[nr].techs.map(tech =>
       <div key={tech.name} className="tech">
         <div className="techsName">{tech.name}</div>
-        <div><FontAwesomeIcon icon={tech.img} size="2x" color={tech.color} /></div>
+        <div><FontAwesomeIcon icon={tech.img}
+          size="2x"
+          color={tech.color} /></div>
       </div >
     )
 
 
     return (
       <div className="containerProjekty">
-
         <p>{this.projects[nr].title}</p>
-
         <div className="mainProjekty">
           <div className="monitor">
             <img src={Monitor} alt="" />
@@ -106,18 +104,21 @@ class Projekty extends Component {
               alt="" />
           </div>
         </div>
-
         <div className="techs">
           {techs}
         </div>
         <div className="links">
-          <a href={this.projects[nr].link} target="blank">Zobacz online</a>
-          <a href={this.projects[nr].gitLink} target="blank">Kod na github</a>
+          <a href={this.projects[nr].link}
+            target="blank">Zobacz online</a>
+          <a href={this.projects[nr].gitLink}
+            target="blank">Kod na github</a>
+
         </div>
         <div className="nextProject">
           <button
             onClick={this.projectChange}>
-            Nastepny projekt <FontAwesomeIcon icon={faForward} size="lg" />
+            Nastepny projekt <FontAwesomeIcon
+              icon={faForward} size="lg" />
           </button>
         </div>
       </div>
